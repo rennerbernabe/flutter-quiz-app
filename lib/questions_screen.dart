95d359e53eff548a7be6af9b786472188079780f
rennerbernabe_ <renner.bernabe@cartrack.com>
2024-02-08 16:04:08 +0800

    Add list of questions and answers in questions screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/answer_button.dart';
+import 'package:flutter_quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
-
 
   @override
   State<StatefulWidget> createState() {
@@ -11,8 +12,30 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final currentQuestion = questions[0];
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Question 1');
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.question,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((item) {
+              return AnswerButton(answerText: item, onTap: () {});
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
